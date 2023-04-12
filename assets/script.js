239f5927c83b3d112b7bd22e9f3e68c9e2f655cd
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-12 15:47:00 -0500

    added timer to top of page

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,25 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
 });
+
+
+// GIVEN I am using a daily planner to create a schedule
+function displayTime() {
+    var today = dayjs().format('MMM D, YYYY, h:mm:ss a');
+    $('#currentDay').text(today);
+}
+setInterval(displayTime, 1000);
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+var hoursinDay=["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
