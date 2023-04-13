707ed94857ea20a82802b9020853b442192a2963
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-12 20:14:27 -0500

    added descriptive comments to functions on script.js page

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
 var saveButton = document.querySelectorAll(".saveBtn");
-$(function () {
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-
-});
-
-
+// function displays time using dayjs. it will display the current time and updates every second
 function displayTime() {
     var today = dayjs().format('MMM D, YYYY, h:mm:ss a');
     $('#currentDay').text(today);
 }
 
+// function changes the color of the hour depending on if it is past/present/or future
 function timeColor() {
     var hour = dayjs().hour();
     var currHour = document.getElementsByClassName("time-block");
@@ -34,6 +30,8 @@ function timeColor() {
 
 };
 
+
+// savebutton eventlistener listens to a save button press and sets the value of the time and user input in localstorage
 for (i = 0; i < saveButton.length; i++) {
     saveButton[i].addEventListener("click", function () {
         var time = $(this).siblings('.hour').text();
@@ -42,6 +40,7 @@ for (i = 0; i < saveButton.length; i++) {
     })
 };
 
+// renderPlans sets the value of currHour to the text in the corresponding(this) div with class .hour. It also sets the value of currPlan to the locally stored item value of the corresponding(this) div with class of .hour. It then sets the text of the corresponding(this) div with class of .description to the value of currPlan.
 function renderPlans() {
     $('.hour').each(function() {
         var currHour = $(this).text();
@@ -52,6 +51,8 @@ function renderPlans() {
     })
 }
 
+
+// calls functions so they work properly and sets a timer to update content every second
 renderPlans();
 timeColor();
 displayTime();
